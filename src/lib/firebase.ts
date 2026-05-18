46656a407dc9e55618ec503382aa507ff31837b3
fajmuls <mrachmanfm@gmail.com>
2026-05-18 15:50:30 +0700

    refactor: Inline firebase config
    The firebase configuration is now directly embedded in the code instead of being imported from a JSON file. This simplifies the setup and reduces external dependencies for initialization.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,10 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
-import firebaseConfig from '../../firebase-applet-config.json';
+const firebaseConfig = {
+  apiKey: "AIzaSyDqTovzK2RnLQr8oidUjzyzRD3FTGHgR0I",
+  authDomain: "fajmuls-daily.firebaseapp.com",
+  projectId: "fajmuls-daily",
+  storageBucket: "fajmuls-daily.firebasestorage.app",
+  messagingSenderId: "1058481681269",
+  appId: "1:1058481681269:web:2587209851faf82837993c"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const authProvider = new GoogleAuthProvider();
